8ed9ae2970db9dd0e19966c61f85be2cc1a05ebd
Legendary-Folah <Folafad15@gmail.com>
2022-09-19 12:40:33 +0100

    abeg oh

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.getElementById('cont');
+const colors = ["#056CF2", "#05SFF2", "#F2E205", "#F28705", "#A62103"];
+
+const getRandomColor = () => colors[Math.floor(Math.random() * colors.length)];
+
+const setColor = (box) => {
+    const color = getRandomColor();
+    box.style.background = color;
+    box.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+};
+
+const removeColor = (box) => {
+    box.style.background = "#1d1d11";
+    box.style.boxShadow = "0 0 2px #000";
+}
